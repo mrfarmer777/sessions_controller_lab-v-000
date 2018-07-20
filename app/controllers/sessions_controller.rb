ede8e4b695ea1c24311d16c2a1aab49acf3eeff2
Matt Farmer <mrfarmer777@gmail.com>
2018-07-20 04:44:43 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    session[:username]=params[:username]
-    redirect_to :root
+    if params[:username].present?
+      session[:username]=params[:username]
+      redirect_to root_path
+    else
+      redirect_to login_path
+    end
   end
 
   def destroy
